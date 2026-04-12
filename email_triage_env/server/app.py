0f128eb0118536f45cd313f8461edbef0307ec86
JJaniel <jjaniel@example.com>
2026-04-12 18:02:21 +0000

    fix: update internal app port to 7860 for full consistency

--- email_triage_env/server/app.py
+++ email_triage_env/server/app.py
@@ -11,7 +11,7 @@ app = create_fastapi_app(
 )
 
 def main():
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    uvicorn.run(app, host="0.0.0.0", port=7860)
 
 if __name__ == "__main__":
     main()
